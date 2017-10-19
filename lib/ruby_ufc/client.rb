24a1c512c65a369e9ec0aa512e0f02fbedb628be
Kiriakos Velissariou <kir.velissariou@gmail.com>
2017-10-20 02:25:06 +0300

    Add methods for fetching all resources

--- lib/ruby_ufc/client.rb
+++ lib/ruby_ufc/client.rb
@@ -3,12 +3,33 @@
 module RubyUfc
   # :nodoc:
   class Client
+    ALLOWED_RESOURCES = %w[fighters octagon_girls events news media].freeze
+
     attr_reader :base_end_point, :version
 
     def initialize
       config_params = YAML.load_file('lib/config/config.yml')
       @base_end_point = config_params['base_end_point']
       @version = config_params['version']
+    end
+
+    ALLOWED_RESOURCES.each do |allowed_resource|
+      define_method(allowed_resource) do
+        url = construct_url(allowed_resource)
+        get_json_response(url)
+      end
+    end
+
+    private
+
+    def construct_url(name)
+      "#{@base_end_point}/#{@version}/#{name}.json"
+    end
+
+    def get_json_response(url)
+      uri = URI(url)
+      response = Net::HTTP.get(uri)
+      JSON.parse(response)
     end
   end
 end
--- test/client_test.rb
+++ test/client_test.rb
@@ -5,7 +5,32 @@ require 'test_helper'
 class ClientTest < Minitest::Test
   def test_that_it_initializes_properly
     client = RubyUfc::Client.new
-    assert_equal client.base_end_point, 'http://ufc-data-api.ufc.com/api/'
+    assert_equal client.base_end_point, 'http://ufc-data-api.ufc.com/api'
     assert_equal client.version, 'v3'
+  end
+
+  def test_that_all_allowed_methods_exist
+    client = RubyUfc::Client.new
+    RubyUfc::Client::ALLOWED_RESOURCES.each do |allowed_resource|
+      assert client.respond_to? allowed_resource
+    end
+  end
+
+  def test_that_an_allowed_method_returns_correct_values
+    client = RubyUfc::Client.new
+    method = RubyUfc::Client::ALLOWED_RESOURCES.first
+
+    response = client.send(method)
+    assert response.class, Array
+  end
+
+  def test_that_construct_url_returns_a_correct_url
+    client = RubyUfc::Client.new
+    base_end_point = client.base_end_point
+    version = client.version
+    resource = RubyUfc::Client::ALLOWED_RESOURCES.first
+
+    url = client.send(:construct_url, resource)
+    assert_equal url, "#{base_end_point}/#{version}/#{resource}.json"
   end
 end
